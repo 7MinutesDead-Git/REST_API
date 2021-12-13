e7651535d9739b1da8f69719bbab85e4d307a965
yayorbitgum <50963144+yayorbitgum@users.noreply.github.com>
2021-12-13 17:54:04 -0600

    Fix uninitialized constant error.

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,7 +1,7 @@
 # Factory Bot for generating test data for Items model.
 FactoryBot.define do
   factory(:item) do
-    name { Faker::StarWars.character }
+    name { Faker::GreekPhilosophers.name }
     done { false }
     todo_id { nil}
   end
